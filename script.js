b61d5798d00f38fe34b029624110ac328e5c64bb
Sam Howard <essjay89@hotmail.co.uk>
2023-05-21 18:17:02 +0100

    added falling cookie

--- script.js
+++ script.js
@@ -4,15 +4,31 @@ let employees = 0;
 let addCookie = document.querySelector(".addCookie");
 let cookieCount = document.querySelector(".cookieCount");
 let addEmployee = document.querySelector(".addEmployee");
+let cookieImage = document.querySelector(".cookieImage");
+
+
 
 function updateCookies () {
     cookieCount.innerHTML = "Cookies: " + cookies;
+}
+
+
+function cookieFallAnimation() {
+    cookieImage.style.top = "100vh"; // Moves the cookie image to the bottom of the screen
+    setTimeout(() => {
+        cookieImage.style.display = "none"; // Hide the cookie image
+        setTimeout(() => {
+            cookieImage.style.display = "block"; // Show the cookie image
+            cookieImage.style.top = "-100px"; // Resets the position of the cookie image to the top
+        }, 100);
+    }, 1000); // Wait for 1 second before resetting the position
 }
 
 addCookie.addEventListener('click', () =>{
     cookies++
     console.log(cookies);
     updateCookies();
+    cookieFallAnimation();
 });
 
 addEmployee.addEventListener('click', () =>{
